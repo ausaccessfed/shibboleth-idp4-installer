07b17cbbdea7041d21431807677c0bd2996f3fea
trsau <t.smith@aaf.edu.au>
2020-08-25 06:53:40 +0000

    Add DB setup configuration

---
+++ templates/db/shibpid_ddl.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS shibpid (
+    localEntity VARCHAR(255) NOT NULL,
+    peerEntity VARCHAR(255) NOT NULL,
+    persistentId VARCHAR(50) NOT NULL,
+    principalName VARCHAR(50) NOT NULL,
+    localId VARCHAR(50) NOT NULL,
+    peerProvidedId VARCHAR(50) NULL,
+    creationDate TIMESTAMP NOT NULL,
+    deactivationDate TIMESTAMP NULL,
+    PRIMARY KEY (localEntity, peerEntity, persistentId),
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
