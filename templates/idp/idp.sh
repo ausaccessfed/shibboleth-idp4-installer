9c30a6693bb3129ed4364dfd72932b734d5be791
trsau <t.smith@aaf.edu.au>
2020-11-02 06:07:30 +0000

    Add new IdP startup script

---
+++ templates/idp/idp.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+usage()
+{
+    echo "Usage: ${0##*/} {start|stop}"
+    exit 1
+}
+
+[ $# -eq 1 ] || usage
+
+case "$1" in
+  start)
+    rm -rf {{ install_base }}/jetty/tmp/*
+    /bin/bash -c "{{ install_base }}/jetty/current/bin/jetty.sh -d start"
+
+    ;;
+
+  stop)
+    /bin/bash -c "{{ install_base }}/jetty/current/bin/jetty.sh stop"
+    rm -rf {{ install_base }} /jetty/tmp/*
+    
+    ;;
+
+  *)
+    usage
+
+    ;;
+esac
+
+exit 0 
